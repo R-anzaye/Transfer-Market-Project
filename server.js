3604c76894e1162163950cd7a960f94f0b8c556f
R-anzaye <162318237+R-anzaye@users.noreply.github.com>
2024-04-11 18:29:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
-server.use('https://transfer-market-project-10.onrender.com/playerData/', router)
+server.use('/', router)
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
